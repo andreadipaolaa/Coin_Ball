2649b57e13b1509803939bdb877d1019108e82d3
Simona <smndrs.sdr@gmail.com>
2022-05-21 13:04:40 +0200

    aggiunto highscore

--- Hole_Ball/Assets/Scripts/GameManager.cs
+++ Hole_Ball/Assets/Scripts/GameManager.cs
@@ -7,12 +7,23 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
 
-    public Text points; 
+    public Text points;
+    public Text highScore;
 
-    //bool gameHasEnded = false;
+    void Start()
+    {
+        highScore.text = PlayerPrefs.GetInt("Highscore", 0).ToString();
+    }
+
+
     public void EndGame(int score)
     {
         points.text = score.ToString() + " POINTS";
+        if (PlayerPrefs.GetInt("Highscore", 0 ) < score)
+        {
+            PlayerPrefs.SetInt("Highscore", score);
+            highScore.text = score.ToString();
+        }
         SceneManager.LoadScene("LoseScreen");
     }
 }
